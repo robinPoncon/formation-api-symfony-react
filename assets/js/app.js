ffeee65edc352b7e02e929a998b8b910de49d485
robinPoncon <poncon.robin@gmail.com>
2021-08-27 11:49:11 +0200

    installation et mise en place de react avec symfony

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,21 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// Les imports importants
+import React from 'react';
+import ReactDOM from "react-dom";
+
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import '../styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+console.log("ça bug maggle");
+
+const App = () => {
+    return <h1>Bonjour à tous !</h1>;
+}
+
+const rootElement = document.querySelector("#app");
+ReactDOM.render(<App/>, rootElement);
